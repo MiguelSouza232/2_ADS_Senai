e7f50239a75ba9b9c5b769621ef7147bba264ded
Miguel <miguelsouza0204@gmail.com>
2025-08-07 21:54:25 -0300

    Projeto de revisão – Calculadora com Interface Gráfica em Java

---
+++ DBE/ProjetoDeRevisao/src/main/java/com/mycompany/projetoderevisao/ProjetoDeRevisao.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetoderevisao;
+
+/**
+ *
+ * @author 25170626
+ */
+public class ProjetoDeRevisao {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
