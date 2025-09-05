5343dc6f29579ec9883e171f80aa6fc4a6b7e9f2
Miguel <miguelsouza0204@gmail.com>
2025-09-04 21:25:37 -0300

    Aula 01 de Programação Orientada a Objetos

---
+++ DBE/Aula2_IntroducaoPOO/src/main/java/com/mycompany/aula2_introducaopoo/Aula2_IntroducaoPOO.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.aula2_introducaopoo;
+
+/**
+ *
+ * @author 25170626
+ */
+public class Aula2_IntroducaoPOO {
+
+    public static void main(String[] args) {
+        String nomeAluno1 = "João";
+        int idadeAluno1 = 20;
+        double notaAluno1 = 8.5;
+
+        String nomeAluno2 = "Maria";
+        int idadeAluno2 = 22;
+        double notaAluno2 = 9.0;
+
+// Imagine fazer isso para 100 alunos!
+    }
+}
---
+++ DBE/Aula2_IntroducaoPOO/src/main/java/com/mycompany/aula2_introducaopoo/Exemplo01POO.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.aula2_introducaopoo;
+
+/**
+ *
+ * @author 25170626
+ */
+public class Exemplo01POO {
+
+    public static void main(String[] args) {
+        ExemploPOOAluno aluno1 = new ExemploPOOAluno();
+        aluno1.nome = "João";
+        aluno1.idade = 20; 
+        aluno1.nota = 8.5;
+
+        ExemploPOOAluno aluno2 = new ExemploPOOAluno();
+        aluno2.nome = "Maria";
+        aluno2.idade = 22;
+        aluno2.nota = 9.0;
+    }
+}
