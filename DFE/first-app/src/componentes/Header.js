f3fba60e333af31bf8bda5659bba126d0a301ac5
Miguel <miguelsouza0204@gmail.com>
2025-11-03 20:08:48 -0300

    Header responsivo com Node

--- DFE/first-app/src/componentes/Header.js
+++ DFE/first-app/src/componentes/Header.js
@@ -1,14 +1,28 @@
+import { useState } from 'react';
 import '../App.css';
 import logo from '../assets/SubLogoCelsinho.png';
 
 export default function Header() {
+  const [menuAberto, setMenuAberto] = useState(false);
+
+  const alternarMenu = () => {
+    setMenuAberto(!menuAberto);
+  };
+
   return (
     <header>
       <div className="divHeader">
         <img src={logo} alt="Dj Celsinho" className="logo" />
-        <nav>
+
+        {/* Botão hamburger para mobile) */}
+        <button className="menu-toggle" onClick={alternarMenu}>
+          ☰
+        </button>
+
+        {/* Menu de navegação com condicional */}
+        <nav className={menuAberto ? 'menu ativo' : 'menu'}>
           <ul>
-            <li>Home</li>
+            <li><a href="index.html">Home</a></li>
             <li><a href="servicos.html">Serviços</a></li>
             <li><a href="contato.html">Contato</a></li>
             <li><a href="encomendas.html">Encomendas</a></li>
